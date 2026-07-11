0444bf3879c46e297daf9fabbadf36fc65ca619b
Digital Hub SD1 <digitalhub-sd1@sunway.com.my>
2026-07-11 20:16:49 +0800

    Fix Tailwind production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/**/*.blade.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
         },
     },
